97a690fabcff04fa8c2931e9ed65dc7368e7cedb
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2024-02-12 15:51:05 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import React,{useState} from 'react'
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
+  const [mode,setMode]=useState('dark');
+  const [alert,setAlert]=useState(null);
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    }) 
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  }
+  
+  const toggleMode=()=>{
+    if(mode ==='light'){
+      setMode('dark')
+      showAlert("Dark mode has been enabled","success")
+      document.title="TextUtils - Dark Mode";
+      setInterval(() => {
+        document.title="TextUtils is Amazing Mode";
+      }, 200);
+      setInterval(() => {
+        document.title="Install TextUtils Now";
+      }, 500);
+    }else{
+      setMode('light')
+      showAlert("Light mode has been enabled","success")
+      document.title="TextUtils - Light Mode";
+    }
+  }  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Router>
+    <Navbar title="TextUtils" about="About" mode={mode} toggleMode={toggleMode}/> 
+    {/* <Navbar/> */}
+    <Alert alert={alert}/>
+    <div className="container">
+      <Routes>
+          <Route exact path="/" element={<TextForm heading="Enter Text To analyze" showAlert={showAlert}/>}/>
+          <Route exact path="/About" element={<About/>}/>
+      </Routes>
+        
+    {/* <About/> */}
     </div>
+    </Router>
+    </>
   );
 }
-
 export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,56 @@
+import React,{useState} from 'react'
+
+export default function Testform(props) {
+  const [text,setText]= useState("Enter text here")
+
+  const onChangeHandler=(event)=>{
+    setText(event.target.value)
+  }
+
+  const toUpper=()=>{
+    let newText=text.toUpperCase()
+    setText(newText)
+    props.showAlert("Converted to Uppercase!", "success");
+  }
+
+  const toLower=()=>{
+    let newText=text.toLowerCase()
+    setText(newText)
+    props.showAlert("Converted to Lowercase!", "success");
+  }
+  const clear=()=>{
+    setText("");
+    props.showAlert("Text Cleared!", "success");
+  }
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text); 
+    props.showAlert("Copied to Clipboard!", "success");
+    props.showAlert("Copied to Clipboard!", "success");
+}
+const handleExtraSpaces = () => {
+  let newText = text.split(/[ ]+/);
+  setText(newText.join(" "));
+  props.showAlert("Extra spaces removed!", "success");
+  props.showAlert("Extra spaces removed!", "success");
+}
+  return (
+    <div>
+        <div className="mb-3">
+          <h1>{props.heading} </h1>
+            <textarea className="form-control" value={text} onChange={onChangeHandler} id="exampleFormControlTextarea1" rows="15"></textarea>
+            </div>
+            <button className="btn btn-primary mx-2" onClick={toUpper}>Convert to uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={toLower}>Convert to lowercase</button>
+            <button className="btn btn-primary mx-2" onClick={clear}>Clear Text</button>
+            <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy to clipboard</button>
+            <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        <div>
+          <h2>Your text Summary</h2>
+          <p>{text===""?"0":text.split(" ").length} words and {text.length} Character</p>
+          <p>{.008 *(text===""?"0":text.split(" ").length)} Minutes read</p>
+          <h2>Preview</h2>
+          <p>{text.length>0?text:'Enter your text'}</p>
+        </div>
+    </div>
+  )
+}
